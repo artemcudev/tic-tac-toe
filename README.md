c25991380e73e60d18230a7198ad8c61053cd6ff
artemcudev <artem.c@cudev.org>
2018-03-06 13:17:31 +0200

    Refactoring of the code

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 Simple 3x3 board game for two players
 
+Demo: http://x2chat.com/tic-tac-toe/
+
 <b>How to start:</b>
 
 Clone project and open index.html in browser
-
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 function TicTacToe() {
 	this.rows = document.querySelectorAll("#board button");
+	this.startButton = document.getElementById("start");
+	this.step = 0;
+	var parent = this;
 	this.markers = {
 		x: {
 			name: "X",
@@ -10,8 +13,6 @@ function TicTacToe() {
 			values: ""
 		}
 	};
-	this.step = 0;
-	var parent = this;
 
 	this.clearBoard = function() {
 		this.rows.forEach(function(item) {
@@ -28,6 +29,15 @@ function TicTacToe() {
 		document.getElementById("notification").innerText = text;
 	};
 
+	this.getMarker = function() {
+		return (this.step % 2 ? this.markers.o : this.markers.x);
+	};
+
+	this.setCaptions = function() {
+		document.getElementById('step').innerText = this.step;
+		this.notificastion('Player ' + this.getMarker().name);
+	};
+
 	this.newGame = function() {
 		this.clearBoard();
 		this.step = 0;
@@ -40,75 +50,32 @@ function TicTacToe() {
 		this.setCaptions();
 	};
 
-	this.getMarkerName = function() {
-		return (this.step % 2 ? this.markers.o : this.markers.x);
-	};
-
-	this.setCaptions = function() {
-		document.getElementById('step').innerText = this.step;
-		this.notificastion('Player ' + this.getMarkerName().name);
-	};
-
-	this.rows.forEach(function(item) {
-		item.onclick = function(e) {
-			if ( ! item.innerHTML) {
-				var marker = parent.getMarkerName().name;
-				parent.getMarkerName().values += item.value;
-				item.innerHTML = marker;
-				item.disabled = true;
-				parent.nextStep();
-				parent.saveData();
-				parent.checkWinners();
-			}
-		};
-	});
-
-	this.saveData = function() {
-		var hash = '';
-		for (var key in this.markers) {
-			var marker = this.markers[key];
-			if (marker.values) {
-				hash += encodeURI(key + '=' + marker.values + ';');
-			}
-		}
-		location.hash = hash;
-	};
-
-	this.play = function() {
-		this.setCaptions();
-		if (location.hash) {
-			try {
-				var hash = decodeURI(location.hash).replace('#', '').split(";");
-				for (var key in hash) {
-					var value = hash[key].split('=');
-					if (value[1]) {
-						for (var key in this.markers) {
-							if (key == value[0]) {
-								this.markers[key].values = value[1];
-								var values = value[1].split("");
-								for (var i in values) {
-									var value = values[i];
-									var button = this.getButton(value);
-									button.innerText = this.markers[key].name;
-									button.disabled = true;
-									this.nextStep();
-								}
-							}
-						}
-					}
-				}
-				parent.checkWinners();
-			} catch (e) {
-				return false;
-			}
-		}
-	};
-
 	this.getButton = function(row) {
 		return document.querySelector("#board button[value=" + row + "]");
 	};
 
+	this.checkLine = function (row1, row2, row3) {
+		value1 = this.getButton(row1).innerText;
+		value2 = this.getButton(row2).innerText;
+		value3 = this.getButton(row3).innerText;
+		if (value1 !== "" && value1 === value2 && value2 === value3) {
+			this.notificastion('Player ' + value1 + ' win');
+			this.getButton(row1).classList.add("win");
+			this.getButton(row2).classList.add("win");
+			this.getButton(row3).classList.add("win");
+			this.rows.forEach(function(item, i) {
+				item.disabled = true;
+			});
+		}
+	};
+
+	// fast solution
 	this.checkWinners = function() {
+		// Minimum number of steps to win
+		if (this.step < 5) {
+			return false;
+		}
+
 		// horizontal
 		this.checkLine("a", "b", "c");
 		this.checkLine("d", "e", "f");
@@ -124,23 +91,70 @@ function TicTacToe() {
 		this.checkLine("g", "e", "c");
 	};
 
-	this.checkLine = function (row1, row2, row3) {
-		value1 = this.getButton(row1).innerText;
-		value2 = this.getButton(row2).innerText;
-		value3 = this.getButton(row3).innerText;
-		if (value1 !== "" && value1 === value2 && value2 == value3) {
-			this.notificastion('Player ' + value1 + ' win');
-			this.getButton(row1).classList.add("win");
-			this.getButton(row2).classList.add("win");
-			this.getButton(row3).classList.add("win");
-			this.rows.forEach(function(item, i) {
-				item.disabled = true;
-			});
+	this.rows.forEach(function(item) {
+		item.onclick = function(e) {
+			if (item.innerHTML) {
+				return false;
+			}
+			var marker = parent.getMarker().name;
+			parent.getMarker().values += item.value;
+			item.innerHTML = marker;
+			item.disabled = true;
+			parent.nextStep();
+			parent.saveData();
+			parent.checkWinners();
+		};
+	});
+
+	this.saveData = function() {
+		var hash = '';
+		for (var key in this.markers) {
+			var marker = this.markers[key];
+			if (marker.values) {
+				hash += encodeURI(key + '=' + marker.values + ';');
+			}
+		}
+		location.hash = hash;
+	};
+
+	this.getData = function() {
+		if (location.hash) {
+			try {
+				var hash = decodeURI(location.hash).replace('#', '').split(";");
+				for (var key in hash) {
+					var value = hash[key].split('=');
+					if (!value[1]) {
+						continue;
+					}
+					for (var key in this.markers) {
+						if (key != value[0]) {
+							continue;
+						}
+						this.markers[key].values = value[1];
+						var values = value[1].split("");
+						for (var i in values) {
+							var value = values[i];
+							var button = this.getButton(value);
+							button.innerText = this.markers[key].name;
+							button.disabled = true;
+							this.nextStep();
+						}
+					}
+				}
+				parent.checkWinners();
+			} catch (e) {
+				return false;
+			}
 		}
 	};
 
-	document.getElementById("start").onclick = function() {
-		parent.newGame();
+	this.play = function() {
+		this.setCaptions();
+		this.getData();
+	};
+
+	this.startButton.onclick = function() {
+		game.newGame();
 	};
 }
 
